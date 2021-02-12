04db21d894b52a426690c57d212d96e845bb7114
oksanaHulyaeva <hulyaevaoksana@gmail.com>
2021-02-12 21:03:01 +0300

    feat: init, add data, cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Data from './components/Data';
+import './App.scss';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {
+        Data.map(card => {
+          return (
+            <div className={"card"} key={card.id}>
+              <div className={"card__gallery"}>
+                <img src={card.img1} alt="" />
+                <img src={card.img2} alt="" />
+              </div>
+              <div className={"card__controls"}>
+                <div className={"checkbox"}></div>
+                <div className={"elipsis-icon"}></div>
+                
+              </div>
+              <div className={"card__profile"}>
+                <a href="/" className={"profile-link"}>
+                  <div className={"profile-avatar"}>
+                    <img src={card.avatar} alt="" width="60" height="60" />
+                  </div>
+                  <p className={"profile-name"}>{card.name}</p>
+                  <p className={"profile-description"}>{card.descr}</p>
+                  <p className={"profile-content"}>{card.content}</p>
+                  <hr />
+                </a>
+              </div>
+              <div className={"card__socials"}></div>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
---
+++ src/components/Data.js
@@ -0,0 +1,15 @@
+const Data = [
+  {
+    name: "Jenny Wilson",
+    id: 1,
+    avatar: "https://i.postimg.cc/g0z4Nb9Z/Avatar.png",
+    img1: "https://i.postimg.cc/BQvnsC8D/Image-1.png",
+    img2: "https://i.postimg.cc/JnPCd95F/Image-2.png",
+    descr: "Female, 37",
+    content: "Music, TV, Film & Animation",
+    socials: [{insta: "188.4M"}, {fb: "93.4M"}, {twitter: "11.5M"}]
+  },
+
+];
+
+export default Data;
